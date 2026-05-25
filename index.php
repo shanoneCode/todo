760050d4fe5abc97012e393eb76d80bafddad09c
shanoneCode <shanonedeffo@gmail.com>
2026-05-25 18:42:22 +0100

    deploy

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 // Connexion à la base de données
 require_once __DIR__ . '/config/db.php';
-$pdo = new PDO('mysql:host=localhost;dbname=tododb;charset=utf8', 'root', '');
+// $pdo = new PDO('mysql:host=localhost;dbname=tododb;charset=utf8', 'root', '');
 
 // Traitement des actions reçues en POST
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
